e4752e61336166a1993e7735af1c6312b8319a88
Aimer Neige <aimer.neige.soft@gmail.com>
2024-05-20 00:42:35 +0800

    better ui

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,95 +1,96 @@
 <template>
   <v-container class="d-flex justify-center align-center fill-height">
     <v-row justify="center">
-      <v-card :style="{ width: '60%' }">
-        <v-toolbar>
-          <v-toolbar-title>Kuroko 短链接</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="toggleTheme">
-            <v-icon>mdi-brightness-6</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <!-- <v-card-title>  </v-card-title> -->
-        <v-card-title>
-          <v-container>
-            <v-row justify="center">
-              <v-col cols="6">
-                <v-img
-                  height="300"
-                  src="../assets/kuroko.png"
-                  aspect-ratio="1/1"
-                  alt="Kuroko"
-                ></v-img>
-              </v-col>
-            </v-row>
-            <v-row justify="center"> 将链接通过十一维进行空间移动！ </v-row>
-          </v-container>
-        </v-card-title>
-        <v-card-actions>
-          <v-container>
-            <v-row>
-              <v-col cols="10">
-                <v-form ref="form">
-                  <v-text-field
-                    label="你的长链接"
-                    :loading="loading"
-                    variant="outlined"
-                    clear-icon="mdi-close-circle"
-                    v-model="originLink"
-                    hint="在这里输入需要缩短的链接"
-                    @keydown.enter="shortLink"
-                    :rules="[rules.required, rules.urllink]"
-                    clearable
-                  >
-                  </v-text-field>
-                  <v-text-field v-show="false"
-                    >https://github.com/vuetifyjs/vuetify/issues/4302
-                  </v-text-field>
-                </v-form>
-              </v-col>
-              <v-col cols="2">
-                <v-btn
-                  color="primary"
-                  size="x-large"
-                  variant="flat"
-                  block
-                  @click="shortLink"
-                  >空间移动
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-actions>
-        <v-divider v-if="shortedList.length > 0"></v-divider>
-        <v-card v-if="shortedList.length > 0">
-          <v-alert v-model="alert" text="短链接已被复制到剪切板！"></v-alert>
-          <v-slide-y-transition class="py-0" tag="v-list" group>
-            <template v-for="(shorted, index) in shortedList" :key="index">
-              <v-divider
-                v-if="index !== 0"
-                :key="`${index}-divider`"
-              ></v-divider>
-              <v-list-item>
-                <template v-slot:prepend>
-                  <v-btn variant="text" icon @click="copyToClipboard(shorted)">
-                    <v-icon> mdi-clipboard-text-multiple-outline </v-icon>
+      <v-col cols="12" md="9" lg="6">
+        <v-card class="elevation-12 rounded-xl pa-12" max-width="1024px">
+          <v-row justify="center">
+            <v-col cols="6">
+              <v-img
+                height="260"
+                src="../assets/kuroko.png"
+                aspect-ratio="1/1"
+                alt="Kuroko"
+                cover
+                class="rounded-xl"
+              ></v-img>
+            </v-col>
+          </v-row>
+          <v-card-title class="text-center text-h5">
+            Kuroko 短链接
+          </v-card-title>
+          <v-card-subtitle class="text-center text-h6">
+            将链接通过十一维进行空间移动！
+          </v-card-subtitle>
+          <v-card-actions>
+            <v-container>
+              <v-row>
+                <v-col cols="10">
+                  <v-form ref="form">
+                    <v-text-field
+                      label="你的长链接"
+                      :loading="loading"
+                      variant="outlined"
+                      clear-icon="mdi-close-circle"
+                      v-model="originLink"
+                      hint="在这里输入需要缩短的链接"
+                      @keydown.enter="shortLink"
+                      :rules="[rules.required, rules.urllink]"
+                      clearable
+                    >
+                    </v-text-field>
+                    <v-text-field v-show="false"
+                      >https://github.com/vuetifyjs/vuetify/issues/4302
+                    </v-text-field>
+                  </v-form>
+                </v-col>
+                <v-col cols="2">
+                  <v-btn
+                    color="primary"
+                    size="x-large"
+                    variant="flat"
+                    block
+                    @click="shortLink"
+                    >空间移动
                   </v-btn>
-                </template>
-                <v-list-item-title>
-                  <span>{{ shorted }}</span>
-                </v-list-item-title>
-                <template v-slot:append>
-                  <v-expand-x-transition>
-                    <v-btn variant="text" icon @click="deleteLink(index)">
-                      <v-icon color="error">mdi-delete</v-icon>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-actions>
+          <v-divider v-if="shortedList.length > 0"></v-divider>
+          <v-card v-if="shortedList.length > 0">
+            <v-alert v-model="alert" text="短链接已被复制到剪切板！"></v-alert>
+            <v-slide-y-transition class="py-0" tag="v-list" group>
+              <template v-for="(shorted, index) in shortedList" :key="index">
+                <v-divider
+                  v-if="index !== 0"
+                  :key="`${index}-divider`"
+                ></v-divider>
+                <v-list-item>
+                  <template v-slot:prepend>
+                    <v-btn
+                      variant="text"
+                      icon
+                      @click="copyToClipboard(shorted)"
+                    >
+                      <v-icon> mdi-clipboard-text-multiple-outline </v-icon>
                     </v-btn>
-                  </v-expand-x-transition>
-                </template>
-              </v-list-item>
-            </template>
-          </v-slide-y-transition>
+                  </template>
+                  <v-list-item-title>
+                    <span>{{ shorted }}</span>
+                  </v-list-item-title>
+                  <template v-slot:append>
+                    <v-expand-x-transition>
+                      <v-btn variant="text" icon @click="deleteLink(index)">
+                        <v-icon color="error">mdi-delete</v-icon>
+                      </v-btn>
+                    </v-expand-x-transition>
+                  </template>
+                </v-list-item>
+              </template>
+            </v-slide-y-transition>
+          </v-card>
         </v-card>
-      </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
